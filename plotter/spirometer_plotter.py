5a3b87dc1eee1a40f172dd1904cee32caa7d4e70
luoa <luoa@uw.edu>
2018-05-27 11:37:19 -0700

    rr sensor code

--- plotter/spirometer_plotter.py
+++ plotter/spirometer_plotter.py
@@ -15,6 +15,11 @@ import seaborn                      # importing automatically makes plotting pre
 BUFFER_SIZE = 3
 SERIAL_LOCK = threading.Lock()
 
+LO_THRESHOLD = 50
+HI_THRESHOLD = 80
+TRIGGER_SIZE = 500
+MAX_TRIGGER_TIME = 150
+
 # dummy serial device that returns a sin wave
 # for testing mainly
 class DummySerial():
@@ -23,12 +28,43 @@ class DummySerial():
         self.rate = 100
 
     def readline(self):
-        v = math.sin(self.time)
+        v = abs(math.sin(self.time)) * 500
 
         self.time = (self.time + 0.01) % (2 * math.pi)
         time.sleep(1.0/self.rate)
 
         return str(v).encode('utf-8')
+
+
+# opesn a file with data and just repeats the data in there over and over again
+class FileSerial():
+    def __init__(self, file_name):
+        self.time = 0
+        self.rate = 100
+        self.data = []
+        self.index = 0
+
+        f = open(file_name, 'r')
+        lines = f.readlines()
+        for i, line in enumerate(lines):
+            try:
+                v = float(line[0:len(line) - 2])
+                self.data.append(min(v, 800))
+
+            except ValueError:
+                print('Corrupted Data: ', line)
+
+        print('Number of data points: ', len(self.data))
+
+    def readline(self):
+        v = self.data[self.index]
+
+        self.index = (self.index + 1) % len(self.data)
+
+        time.sleep(1.0/self.rate)
+
+        return str(v).encode('utf-8')
+
 
 
 class ReadSerialThread(threading.Thread):
@@ -46,92 +82,134 @@ class ReadSerialThread(threading.Thread):
 
 
 class Plotter():
-    def __init__(self, buff, MAX_SIZE_BUFFER = 300, flow_func = lambda f: f / 2830.350827,
-                       dt = 1, fmax = 300000, fmin = -150000, vmax = 40000):
+    def __init__(self, buff, MAX_SIZE_BUFFER = 1300, signal_func = lambda f: f, dt = 60 / 1300, smin = 0, smax = 800):
         self.dt = dt        
         self.buff = buff
-        self.flow_func = flow_func
+        self.signal_func = signal_func
 
-        self.data_vt_v = numpy.array([float(0) for i in range(MAX_SIZE_BUFFER)])
-        self.data_vt_t = numpy.array([i * dt for i in range(MAX_SIZE_BUFFER)])
+        '''
+        Buffers for plotting
+        '''
 
-        self.data_fv_f = numpy.array([float(0) for i in range(MAX_SIZE_BUFFER)])
-        self.data_fv_v = numpy.array([float(0) for i in range(MAX_SIZE_BUFFER)])
+        self.data_signal_y = numpy.array([float(0) for i in range(MAX_SIZE_BUFFER)])
+        self.data_signal_t = numpy.array([i * dt for i in range(MAX_SIZE_BUFFER)])
 
-        self.figure = plt.figure(figsize=(8, 16))
-        self.vt = plt.subplot(211)
-        self.vt.set_title('Volume vs. Time')
-        self.vt.set_xlabel('Time(s)')
-        self.vt.set_ylabel('Volume Exhaled (L)')
-
-        self.fv = plt.subplot(212)
-        self.fv.set_title('Flow vs. Volume')
-        self.fv.set_xlabel('Volume Exhaled (L)')
-        self.fv.set_ylabel('Flow (L / s)')
-        self.figure.tight_layout()
-
-        self.vt.set_ylim([0, vmax])
-        self.fv.set_ylim([fmin, fmax])
-        self.fv.set_xlim([0, vmax])
-
-        self.line_vt, = self.vt.plot(self.data_vt_t, self.data_vt_v, 'r-') 
-        self.line_fv, = self.fv.plot(self.data_fv_v, self.data_fv_f, 'r-') 
+        self.data_breaths_y = numpy.array([float(0) for i in range(MAX_SIZE_BUFFER)])
+        self.data_breaths_t = numpy.array([float(0) for i in range(MAX_SIZE_BUFFER)])
 
         self.tick = 0
 
-        self.follower_vt, = self.vt.plot([self.tick, self.tick], [0, vmax], 'g-')
+        self.cur_sum = 0
+        self.triggered = False
+        self.cur_n = 0
+        self.num_breaths = 0
+        self.v = 0
+        self.time_triggered = 0
+
+        '''
+        PLOTS
+        '''
+        self.figure = plt.figure(figsize=(8, 16))
+        self.signal = plt.subplot(211)
+        self.signal.set_title('Raw Signal')
+        self.signal.set_xlabel('Time(s)')
+        self.signal.set_ylabel('Signal (ADC units)')
+
+        self.breaths = plt.subplot(212)
+        self.breaths.set_title('Count of breaths')
+        self.breaths.set_xlabel('Time(s)')
+        self.breaths.set_ylabel('# Breaths')
+        self.figure.tight_layout()
+
+        self.signal.set_ylim([smin, smax])
+        self.signal.set_xlim([0, dt * MAX_SIZE_BUFFER])
+        self.breaths.set_ylim([smin, smax])
+        self.breaths.set_xlim([0, dt * MAX_SIZE_BUFFER])
+
+        '''
+        Line animations we will update with buffers
+        '''
+        self.line_signal, = self.signal.plot(self.data_signal_t, self.data_signal_y, 'r-') 
+        self.line_breaths, = self.breaths.plot(self.data_breaths_t, self.data_breaths_y, 'r-') 
+        self.follower, = self.signal.plot([self.tick, self.tick], [0, smax], 'g-')
         #self.follower_fv, = self.fv.plot([self.tick, self.tick], [fmin, fmax], 'g-')
+
 
         self.figure.show()
 
 
     def update(self):
-        cur_f = self.flow_func(sum(self.buff) / len(self.buff))
+        raw_signal = self.signal_func(sum(self.buff) / len(self.buff))
+        self.v = raw_signal * 0.0549 + self.v * 0.945 
+        signal = self.v
+        #print(signal)
 
         if self.tick == 0:
-            print('FVC (L):', max(self.data_vt_v))
+            print('Start')
+            print('Num breaths: ', self.num_breaths)
+            self.data_signal_y.fill(0.0)
+            self.data_signal_t.fill(0.0)
+            self.data_breaths_y.fill(0.0)
+            self.data_breaths_t.fill(0.0)
 
-            # calculate FEV1
-            start = -10000
-            for i in range(len(self.data_vt_v)):
-                if self.data_vt_v[i] > 0.1:
-                    start = i
-                    break
-            FEV_i = min(round(start + 1 / self.dt), len(self.data_vt_v) - 1)
-            if FEV_i > 0:
-                print('FEV1 (L):', self.data_vt_v[FEV_i])
-                print('FEV1/FVC:', self.data_vt_v[FEV_i] / max(self.data_vt_v))
-            else:
-                print('FEV1 (L) could not be estimated!')
-            print()
+            self.cur_sum = 0
+            self.triggered = False
+            self.cur_n = 0
+            self.num_breaths = 0
+            self.time_triggered = 0
 
-            self.data_vt_v.fill(0.0)
-            self.data_fv_f.fill(0.0)
-            self.data_fv_v.fill(0.0)
         else:
-            self.data_fv_f[self.tick] = cur_f
-            # don't count inspiration toward volume
-            #if cur_f < 0:
-            #    cur_f = 0
-            dV = cur_f * 1.0 * self.dt
-            self.data_vt_v[self.tick] = self.data_vt_v[self.tick - 1] + dV
+            self.data_signal_y[self.tick] = signal
+            self.data_signal_t[self.tick] = self.data_signal_t[self.tick - 1] + 1 * self.dt
 
-            self.data_fv_v[self.tick] = self.data_vt_v[self.tick]
+            '''
+            Schmitt Trigger processing
+            '''
+            v = 0
+            if not self.triggered:
+                self.cur_n += 1
 
-        self.tick = (self.tick + 1) % len(self.data_vt_v)
+                if signal >= HI_THRESHOLD + self.cur_sum / self.cur_n:
+                    v = self.cur_sum / self.cur_n + TRIGGER_SIZE
+                    self.num_breaths += 1
+                    self.triggered = True
+                else:
+                    v = self.cur_sum / self.cur_n
 
+                self.cur_sum += signal
+                self.time_triggered = 0
 
-        # only plot increases in volume for vt graph
-        data_fv_v_positive = numpy.copy(self.data_vt_v)
-        for i in range(1, self.tick + 1):
-            data_fv_v_positive[i] = max(data_fv_v_positive[i - 1], data_fv_v_positive[i])
-        self.line_vt.set_ydata(data_fv_v_positive)
+            if self.triggered:
+                v = self.cur_sum / self.cur_n + TRIGGER_SIZE
+                
+                if signal <= LO_THRESHOLD + self.cur_sum / self.cur_n:
+                    self.triggered = False 
+                
+                self.time_triggered += 1
+                if self.time_triggered > MAX_TRIGGER_TIME:
+                    self.triggered = False
+                    self.cur_sum = signal
+                    self.cur_n = 1
+                    self.time_triggered = 0
+                    
 
-        self.line_fv.set_ydata(self.data_fv_f[0:self.tick])
-        self.line_fv.set_xdata(self.data_fv_v[0:self.tick])
+            self.data_breaths_y[self.tick] = v
+            self.data_breaths_t[self.tick] = self.data_signal_t[self.tick - 1] + 1 * self.dt
+                
 
-        self.follower_vt.set_xdata([self.data_vt_t[self.tick], self.data_vt_t[self.tick]])
-        #self.follower_fv.set_xdata([self.data_fv_v[self.tick], self.data_fv_v[self.tick]])
+        self.tick = (self.tick + 1) % len(self.data_signal_y)
+    
+        # updating raw signal graph
+        self.line_signal.set_ydata(self.data_signal_y[0:self.tick])
+        self.line_signal.set_xdata(self.data_signal_t[0:self.tick])
+
+        # updating breaths graph
+        self.line_breaths.set_ydata(self.data_breaths_y[0:self.tick])
+        self.line_breaths.set_xdata(self.data_breaths_t[0:self.tick])
+
+        # updating cool follower
+        self.follower.set_xdata([self.tick * self.dt, self.tick * self.dt])
+        self.follower.set_ydata([0, 1000])
 
         self.figure.canvas.draw()
 
@@ -141,7 +219,8 @@ def init_serial(baud_rate, device_name, timeout=5):
     ports = list(serial.tools.list_ports.comports())
 
     for port in ports:
-        if device_name in port.description:
+        print (port)
+        if device_name in str(port):
             return serial.Serial(port.device, baud_rate, timeout=timeout)
     
     print('ERROR: could not find port of device with name', device_name)
@@ -177,8 +256,8 @@ if __name__ == "__main__":
     Because there is no pointer redirection nonsense -- better l1/l2 util.
     '''
     buff = numpy.array([0.0] * BUFFER_SIZE)
-    serial = init_serial(9600, 'Arduino')
-    plotter = Plotter(buff, flow_func = lambda f: f / 2830.350827, dt = 1 / 16.67, fmax = 15, fmin = -3, vmax = 8)
+    serial = FileSerial('haley_7_times_60s_deep_breahts_still.txt')#init_serial(9600, 'rfcomm')
+    plotter = Plotter(buff)
 
     #serial =  DummySerial()
     #plotter = Plotter(buff, fmax = 1, fmin = -1, vmax = 10)
